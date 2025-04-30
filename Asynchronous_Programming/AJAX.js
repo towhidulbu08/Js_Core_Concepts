7555b263052f82a6ab312dc5320d1924418babff
towhidulbu08 <towhidulislam26121997@gmail.com>
2025-04-30 19:51:49 +0600

    Promise pattern of XMLHttpRequest is added

--- Asynchronous_Programming/AJAX.js
+++ Asynchronous_Programming/AJAX.js
@@ -1,33 +1,75 @@
-const url = "https://jsonplaceholder.typicode.com/posts/1";
+const url = "https://jsonplaceholder.typicode.com/postss";
 
-const xhr = new XMLHttpRequest();
+//const xhr = new XMLHttpRequest();
 
 //step-01:
-//console.log(xhr.readyState); // 0
-xhr.open("GET", url);
-//console.log(xhr.readyState); // 1
 
-// xhr.onreadystatechange = function () {
-//   if (xhr.readyState === 4) {
-//     const response = JSON.parse(xhr.response);
-//     console.log(response);
+//xhr.open("GET", url);
+
+// xhr.onload = function () {
+//   if (xhr.status >= 200 && xhr.status < 300) {
+//     const data = JSON.parse(xhr.response);
+//     //console.log(data);
+//     const id = data[3].id;
+
+//     const xhr2 = new XMLHttpRequest();
+//     const url2 = `${url}/${id}`;
+//     xhr2.open("GET", url2);
+//     xhr2.onload = function () {
+//       if (xhr2.status >= 200 && xhr2.status < 300) {
+//         const data2 = JSON.parse(xhr2.response);
+//         console.log(data2);
+//       } else {
+//         console.log("something wrong");
+//       }
+//     };
+//     xhr2.send();
+//   } else {
+//     console.log("something wrong");
 //   }
 // };
-
-xhr.onload = function () {
-  if (xhr.status >= 200 && xhr.status < 300) {
-    const response = JSON.parse(xhr.response);
-    console.log(response);
-  } else {
-    console.log("something wrong");
-  }
-};
 //handing network errors
 
-xhr.onerror = function () {
-  console.log("network error");
-};
+// xhr.onerror = function () {
+//   console.log("network error");
+// };
 
-xhr.send();
+//xhr.send();
 
 //console.log("hello");
+
+// using Promise
+
+function getData(method, url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (xhr.status >= 200 && xhr.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject("something went wrong");
+      }
+    };
+    xhr.onerror = function () {
+      reject("network error");
+    };
+    xhr.send();
+  });
+}
+
+getData("GET", url)
+  .then((response) => {
+    const data = JSON.parse(response);
+    console.log(data);
+    const id = data[3].id;
+    const url2 = `${url}/${id}`;
+    return getData("GET", url2);
+  })
+  .then((data) => {
+    const data2 = JSON.parse(data);
+    console.log(data2);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
